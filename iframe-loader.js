f18b739ca9665fec08c340a4b8e24aabc2746236
TeamTagzTikTok <155990320+TeamTagzTikTok@users.noreply.github.com>
2024-09-01 15:00:51 +0100

    Update iframe-loader.js

--- iframe-loader.js
+++ iframe-loader.js
@@ -1,7 +1,7 @@
 window.onload = function() {
   var iframeContainer = document.getElementById('iframe-container');
   var iframe = document.createElement('iframe');
-  iframe.src = "https://frankietry0.wixsite.com/cloudysoe-vm";
+  iframe.src = "http://frankietry0.wixsite.com/cloudysoe-vm";
   iframe.style = "width:100%; height:100vh; border:none; position:absolute; top:-50px;";
   iframeContainer.appendChild(iframe);
 };
